39f2f084937c60066a6733f888f6317dbd869f11
Valentin <valentin.urbine967@gmail.com>
2023-10-13 01:14:17 -0300

    01:14 - 13-10-2023

--- database/seeders/AdminDefault.php
+++ database/seeders/AdminDefault.php
@@ -22,28 +22,3 @@ class AdminDefault extends Seeder
         ]);
     }
 }
-
-<?php
-
-namespace Database\Seeders;
-
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
-use App\Models\User;;
-
-class AdminDefault extends Seeder
-{
-    /**
-     * Run the database seeds.
-    */
-    public function run(): void
-    {
-        User::create([
-            'name' => 'admin',
-            'email' => 'admin@admin.com',
-            'password' => bcrypt('20406080'),
-            'administrator' => true
-        ]);
-
-    }
-}
